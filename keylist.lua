b16d4b4d4a57efaa6a8ee09d4bb77518208b3e49
tayyabakm450-spec <tayyabakm450@gmail.com>
2026-09-10 22:12:12 +0500

    Increase max_devices for KASHMIRY_PAID_USER

--- keylist.lua
+++ keylist.lua
@@ -28,7 +28,7 @@ return {
         type = "VIP",
         expiry = "2026-09-20",
         valid = true,
-        max_devices = 1,
+        max_devices = 2,
         SLOT = "3"
     },
     ["BLOCKED"] = {
