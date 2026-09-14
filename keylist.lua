dc5987f7373a12e47ee1bde6d407aa343803de22
tayyabakm450-spec <tayyabakm450@gmail.com>
2026-09-14 20:39:10 +0500

    Increase max_devices for KASHMIRY1 from 1 to 2

--- keylist.lua
+++ keylist.lua
@@ -14,7 +14,7 @@ return {
         type = "VIP",
         expiry = "2027-12-31",
         valid = true,
-        max_devices = 1,
+        max_devices = 2,
         SLOT = "1"
     },
     ["AJ_HACKERxTAYYAB_VP"] = {
